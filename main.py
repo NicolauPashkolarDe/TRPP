47055fdc65dc31d6ae3f9ba15950449b0a89023a
NicolauPashkolarDe <105779083+NicolauPashkolarDe@users.noreply.github.com>
2022-05-19 00:30:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,36 @@
 import telebot
+import wikipedia
+import re
 
 bot = telebot.TeleBot("5386042733:AAHD-WrHTfR3_GFACToO3_kfJoPJctqX6SY")
-user_data = {}
-
-class User:
-    def __init__(self, fname):
-        self.fname = fname
-        self.lname = ''
-
-
-@bot.message_handler(commands=['start', 'help'])
-def send_name(message):
-    msg = bot.send_message(message.chat.id, "Введите имя")
-    bot.register_next_step_handler(msg, process_fname_step)
-
-
-def process_fname_step(message):
+wikipedia.set_lang("ru")
+def getwiki(s):
     try:
-        user_id = message.from_user.id
-        user_data[user_id] = User(message.text)
-        msg = bot.send_message(message.chat.id, "Введите фамилию")
-        bot.register_next_step_handler(msg, process_lname_step)
+        ny = wikipedia.page(s)
+        wikitext=ny.content[:1000]
+        wikimas=wikitext.split('.')
+        wikimas = wikimas[:-1]
+        wikitext2 = ''
+        for x in wikimas:
+            if not('==' in x):
+                if(len((x.strip()))>3):
+                   wikitext2=wikitext2+x+'.'
+            else:
+                break
+        wikitext2=re.sub('\([^()]*\)', '', wikitext2)
+        wikitext2=re.sub('\([^()]*\)', '', wikitext2)
+        wikitext2=re.sub('\{[^\{\}]*\}', '', wikitext2)
+        return wikitext2
     except Exception as e:
-        bot.reply_to(message, 'что-то пошло не так')
+        return 'В энциклопедии нет информации об этом'
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'Отправьте мне любое слово, и я найду его значение на Wikipedia')
 
 
-def process_lname_step(message):
-    try:
-        user_id = message.from_user.id
-        user = user_data[user_id]
-        user.lname = message.text
-        bot.send_message(message.chat.id, "Регистрация завершена")
-    except Exception as e:
-        bot.reply_to(message, 'что-то пошло не так')
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    bot.send_message(message.chat.id, getwiki(message.text))
 
 
-bot.enable_save_next_step_handlers(delay=2)
-bot.load_next_step_handlers()
-
-bot.polling(none_stop=True)
+bot.polling(none_stop=True, interval=0)
